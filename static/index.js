5b49a23baf704d051c193f9208df8326a95b36b3
SathyaRaj <sathyarajsaliyan@gmail.com>
2022-01-12 15:12:57 +0530

    css moved into static & icons made avilable Offline

---
+++ static/index.js
@@ -0,0 +1,19 @@
+// window.onscroll=function()
+// {myFunction()};
+// var navbar= document.querySelector("nav");
+// var sticky = navbar.offsetTop;
+// var btn=document.querySelector(".stage");
+// var btn1=document.querySelector(".btn")
+// function myFunction(){
+//     if (window.pageYOffset>=sticky)
+//     {
+//         navbar.classList.add("sticky");
+//         btn.classList.remove("btn1")
+
+//     }else{
+//         navbar.classList.remove("sticky");
+//     }
+    
+// }
+
+  
